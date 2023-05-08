471b874173bee73246a30a32c1127033374d7ae1
brock810 <130688727+brock810@users.noreply.github.com>
2023-05-07 21:58:37 -0500

    another file

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace LINQ
+﻿using System.Security.Cryptography;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string> { "CALL OF DUTY", "Mafia", "Mario", "RedFall" };
+
+            var letters = new List<int>() { 10, 5, 5, 6 };
+
+            var sorted = letters.OrderBy(letters => letters).ToList();
+            
+            letters.Sort();
+
+            foreach (var letter in sorted)
+            {
+                Console.WriteLine(letter.ToString());
+            }
+
         }
     }
 }
